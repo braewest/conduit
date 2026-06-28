5ac5188b0551748e23448f081cdfadcc1a883af7
Braeden West <braedenwest1@gmail.com>
2026-06-27 17:29:26 -0700

    Initial commit + req and res utilities

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+// Collects user request body chunks into a parsed JS object
+function readBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', chunk => body += chunk.toString()); // Chunk data to body
+        req.on('end', () => { // When end of chunks, parse body to JS object
+            try { resolve(JSON.parse(body)); }
+            catch { reject(new Error('Invalid JSON')); }
+        });
+        req.on('error', reject);
+    });
+}
+
+// Sends a JSON response with the given status code
+function sendJSON(res, status, data) {
+    res.writeHead(status, { 'Content-Type': 'application/json' }); // header
+    res.end(JSON.stringify(data)); // data
+}
+
+module.exports = { readBody, sendJSON };
